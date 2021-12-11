1567b0f215e3772e8d97da3bc71a3f742848a917
Corey Wills <cwills788@gmail.com>
2021-12-10 20:00:39 -0600

    added form to add additional tasks

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+const container = document.querySelector('.container')
 const addBtns = document.querySelectorAll('.add')
 const stars = document.querySelectorAll('.star')
 const total = document.querySelector('.total')
 let totalStars = 0
+const submit = document.querySelector('.submit')
+const input = document.getElementById('new-chore')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // console.log(input.value)
+
+  if (input.value !== '') {
+    let div = document.createElement('div')
+    div.setAttribute('class', 'star-container')
+    div.innerHTML = `
+    <h1>${input.value}</h1>
+    <button class="add">Add/Remove Star</button>
+    <div class="star">⭐</div>
+  `
+    container.appendChild(div)
+
+    input.value = ''
+  }
+})
 
 addBtns.forEach((btn, index) => {
   addEventListener('click', (e) => {
@@ -10,5 +32,8 @@ addBtns.forEach((btn, index) => {
     }
     totalStars = document.getElementsByClassName('star active').length
     total.innerHTML = `Total Stars: ${totalStars}`
+    if (totalStars === stars.length) {
+      total.innerHTML = `Total Stars: ${totalStars} Awesome Job!`
+    }
   })
 })
