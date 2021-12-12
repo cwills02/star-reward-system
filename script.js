2fe0237c44ab5bddd0b5178099b31b255408aabb
Corey Wills <cwills788@gmail.com>
2021-12-12 05:17:05 -0600

    added remove star button from each chore on list

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ⭐
+
 const container = document.querySelector('.container')
 // const addBtns = document.querySelectorAll('.add')
 // const stars = document.querySelectorAll('.star')
@@ -9,34 +11,57 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  // console.log(input.value)
 
   if (input.value !== '') {
     let div = document.createElement('div')
     div.setAttribute('class', 'star-container')
     div.innerHTML = `
     <h1>${input.value}</h1>
-    <button class="add">Add/Remove Star</button>
+    <button class="add">+</button>
+    <button class="remove">-</button>
     <div class="star">⭐</div>
   `
     container.appendChild(div)
 
     input.value = ''
   }
-  const addBtns = document.querySelectorAll('.add')
-  const stars = document.querySelectorAll('.star')
-  const total = document.querySelector('.total')
+  let addBtns = document.querySelectorAll('.add')
+  let removeBtns = document.querySelectorAll('.remove')
+  let stars = document.querySelectorAll('.star')
+  let total = document.querySelector('.total')
 
-  addBtns.forEach((btn, index) => {
+  let addBtnsArr = Array.from(addBtns)
+  let removeBtnsArr = Array.from(removeBtns)
+
+  let starsArr = Array.from(stars)
+  starsArr.map((star, index) => star.setAttribute('id', `${index}`))
+
+  addBtnsArr.forEach((btn, index) => {
     addEventListener('click', (e) => {
-      if (e.target === addBtns[index]) {
-        stars[index].classList.toggle('active')
+      if (
+        e.target === btn &&
+        !document.getElementById(`${index}`).classList.contains('star-active')
+      ) {
+        document.getElementById(`${index}`).classList.add('star-active')
       }
-      totalStars = document.getElementsByClassName('star active').length
+      totalStars = document.getElementsByClassName('star-active').length
       total.innerHTML = `Total Stars: ${totalStars}`
       if (totalStars === stars.length) {
-        total.innerHTML = `Total Stars: ${totalStars} Awesome Job!`
+        total.innerHTML = `Total Stars: ${totalStars} <span class="awesome">Awesome Job!</span>`
       }
     })
+  })
+
+  removeBtnsArr.forEach((btn, index) => {
+    addEventListener('click', (e) => {
+      if (
+        e.target === btn &&
+        document.getElementById(`${index}`).classList.contains('star-active')
+      ) {
+        document.getElementById(`${index}`).classList.remove('star-active')
+      }
+    })
+    totalStars = totalStars--
+    total.innerHTML = `Total Stars: ${totalStars}`
   })
 })
